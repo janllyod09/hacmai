1facf9c144feb2205e21217bbbc9c183dff441e1
janllyod09 <bulicjanllyod@gmail.com>
2024-12-09 20:40:41 +0800

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,10 +16,10 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script defer src="assets/app-D-FaURHc.js"></script>
+    <script defer src="build/assets/app-D-FaURHc.js"></script>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="assets/app-DGoPAGMe.css">
+    <link rel="stylesheet" href="build/assets/app-DGoPAGMe.css">
     @livewireStyles
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.5.0/flowbite.min.css" rel="stylesheet">
